62692e7a24457f9b5c437fddd838481d6eaadeea
Robert Gieseke <robert.gieseke@pik-potsdam.de>
2017-02-03 14:41:14 +0100

    Setup tweaks

--- setup.py
+++ setup.py
@@ -1,11 +1,42 @@
+"""
+pymagicc
+--------
+
+Thin Python wrapper around the reduced complexity climate model
+[MAGICC 6](http://magicc.org/).
+
+***Install*** using ::
+
+    pip install pymagicc
+
+Find **usage** instructions in the repository
+<https://github.com/openclimatedata/pymagicc>.
+
+"""
+
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+    def run_tests(self):
+        import pytest
+        pytest.main(self.test_args)
+
 
 setup(
     name='pymagicc',
-    version='0.0.1',
-    description='Thin Python wrapper for the simple '
-                'climate model  MAGICC',
-    classifiers=[
+    version='0.1',
+    description='Thin Python wrapper for the simple climate model  MAGICC',
+    author='Robert Gieseke',
+    author_email='robert.gieseke@pik-potsdam.de',
+    url='https://github.com/openclimatedata/pymagicc',
+    keywords=[],
+        classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
@@ -14,17 +45,11 @@ setup(
         'Programming Language :: Python :: 3.5'
         'Programming Language :: Python :: 3.6'
     ],
-    keywords='',
-    url='https://github.com/openclimatedata/pymagicc',
     license='AGPL',
-    package_data={
-        'pymagicc': ['MAGICC6']
-    },
+    package_data={'pymagicc': ['MAGICC6']},
     include_package_data=True,
     zip_safe=False,
-    install_requires=[
-        'pandas',
-        'f90nml',
-        'pytest'
-    ]
+    install_requires=['pandas', 'f90nml'],
+    tests_require=['pytest'],
+    cmdclass = {'test': PyTest}
 )
