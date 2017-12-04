63291a19bbb6c80f260fabcba7831d2251b1ec57
Robert Gieseke <robert.gieseke@pik-potsdam.de>
2017-12-04 16:49:49 +0100

    Brew-install Python for macOS testing

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-    brew install wine
+    brew install python3 wine
+    python3 -m venv venv
+    ./venv/bin/pip install pytest pytest-cov codecov
+    ./venv/bin/pip .
 else
     sudo dpkg --add-architecture i386
     sudo add-apt-repository ppa:ubuntu-wine/ppa -y
     sudo apt-get update
     sudo apt-get install --no-install-recommends -y wine1.8
+    pip install pytest pytest-cov codecov
 fi
-pip install pytest pytest-cov codecov
