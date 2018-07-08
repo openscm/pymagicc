de51a22dd9af1da1cf07377f76800a0f15138697
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2018-07-08 23:33:32 +0200

    Start tidying up

--- pymagicc/MATLAB_to_rewrite/+Readers/SCEN7FileReader.m
+++
@@ -1,8 +0,0 @@
-classdef SCEN7FileReader < object_oriented_simcap.Readers.MAGICCDataFileReader
-    methods (Access = protected)
-        function make_Variable_header_row_in_header_rows_tmp(self)
-            GAS_cell_idx = strcmp(self.header_rows_tmp,'GAS');
-            self.header_rows_tmp(GAS_cell_idx) = {'Variable'};
-        end
-    end
-end
