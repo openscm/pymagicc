54843804c2e5d4f076a6d2043632d94096c4373c
Robert Gieseke <robert.gieseke@pik-potsdam.de>
2018-07-12 21:39:23 +0200

    Check for Travis environment for notebook tests

--- scripts/test_notebooks.sh
+++ scripts/test_notebooks.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-./venv/bin/pytest --nbval \
+
+PYTEST=./venv/bin/pytest
+
+if [ "$TRAVIS" = true ]; then
+  PYTEST=pytest
+fi
+
+$PYTEST --nbval \
   notebooks/Example.ipynb \
   notebooks/Diagnose-TCR-ECS.ipynb \
 --sanitize notebooks/tests_sanitize.cfg
