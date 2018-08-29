0fa8af1100d9f7b5af6a15bceab9e023479e74b7
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2018-08-29 15:56:07 +0200

    Update files from master and remove MATLAB so Robert does not have a heart attack

--- setup.py
+++ setup.py
@@ -58,7 +58,8 @@ setup(
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6" "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
     packages=["pymagicc"],
     package_data={
