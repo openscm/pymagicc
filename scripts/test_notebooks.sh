5744af2169241c046f74d9ad80b64869a8d2c066
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2018-10-18 18:40:19 +0200

    Make notebooks CI more opinionated

--- scripts/test_notebooks.sh
+++ scripts/test_notebooks.sh
@@ -6,7 +6,10 @@ if [ "$TRAVIS" = true ]; then
   PYTEST=pytest
 fi
 
-$PYTEST --nbval \
-  notebooks/Example.ipynb \
-  notebooks/Diagnose-TCR-ECS.ipynb \
---sanitize notebooks/tests_sanitize.cfg
+# $PYTEST --nbval \
+#   notebooks/Example.ipynb \
+#   notebooks/Diagnose-TCR-ECS.ipynb \
+#   notebooks/Input-Examples.ipynb
+# --sanitize notebooks/tests_sanitize.cfg
+
+$PYTEST --nbval notebooks --sanitize notebooks/tests_sanitize.cfg
