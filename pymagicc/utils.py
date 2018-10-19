4220418d0642c0fb57eb66c401436418be263413
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2018-10-19 11:54:05 +0200

    Update utils

--- pymagicc/utils.py
+++ pymagicc/utils.py
@@ -1,3 +1,13 @@
+"""Module for useful functions that aren't available elsewhere and don't fit with Pymagicc.
+
+When we say "available elsewhere and don't fit with Pymagicc" we mean that they are
+not available to be included as a dependency of Pymagicc nor are they really/solely
+related to running MAGICC. A good test of what belongs here is anything for which one
+thinks, "I need this, I can't find it anywhere else and I would probably use it again
+in another project". One day we may move all these utility functions to another
+project to make it easier for ourselves and others to re-use them.
+"""
+
 from copy import deepcopy
 import re
 
@@ -15,17 +25,68 @@ def _multiple_replace(in_str, rep_dict, compiled_regexp):
     return out_str
 
 
-def _replace_from_replacement_dict(inputs, replacements, inverse=False):
+def apply_string_substitutions(inputs, substitutions, inverse=False):
+    """Apply a number of substitutions to a string(s).
+
+    The substitutions are applied effectively all at once. This means that conflicting
+    substitutions don't interact. Where substitutions are conflicting, the one which
+    is longer takes precedance. This is confusing so we recommend that you look at
+    the examples.
+
+    Parameters
+    ----------
+    inputs: str, list of str
+        The string(s) to which we want to apply the substitutions.
+
+    substitutions: dict
+        The substitutions we wish to make. The keys are the strings we wish to
+        substitute, the values are the strings which we want to appear in the output
+        strings.
+
+    inverse : bool
+        If True, do the substitutions the other way around i.e. use the keys as the
+        strings we want to appear in the output strings and the values as the strings
+        we wish to substitute.
+
+    Returns
+    -------
+    ``type(input)``
+        The input
+
+    Examples
+    --------
+    >>> apply_string_substitutions("Hello JimBob", {"Jim": "Bob"})
+    'Hello BobBob'
+
+    >>> apply_string_substitutions("Hello JimBob", {"Jim": "Bob"}, inverse=True)
+    'Hello JimJim'
+
+    >>> apply_string_substitutions(["Hello JimBob", "Jim says, 'Hi Bob'"], {"Jim": "Bob"})
+    ['Hello BobBob', "Bob says, 'Hi Bob'"]
+
+    >>> apply_string_substitutions(["Hello JimBob", "Jim says, 'Hi Bob'"], {"Jim": "Bob"}, inverse=True)
+    ['Hello JimJim', "Jim says, 'Hi Jim'"]
+
+    >>> apply_string_substitutions("Muttons Butter", {"M": "B", "Button": "Zip"})
+    'Buttons Butter'
+    # Substitutions don't cascade. If they did, Muttons would become Buttons, then the
+    # substitutions "Button" --> "Zip" would be applied and we would end up with
+    # "Zips Butter".
+
+    >>> apply_string_substitutions("Muttons Butter", {"Mutton": "Gutter", "tt": "zz"})
+    'Gutters Buzzer'
+    # Longer substitutions take precedent. Hence Mutton becomes Gutter, not Muzzon.
+    """
     if inverse:
-        replacements = {v: k for k, v in replacements.items()}
-    compiled_regexp = _compile_replacement_regexp(replacements)
+        substitutions = {v: k for k, v in substitutions.items()}
+    compiled_regexp = _compile_replacement_regexp(substitutions)
 
     inputs_return = deepcopy(inputs)
     if isinstance(inputs_return, str):
-        inputs_return = _multiple_replace(inputs_return, replacements, compiled_regexp)
+        inputs_return = _multiple_replace(inputs_return, substitutions, compiled_regexp)
     else:
         inputs_return = [
-            _multiple_replace(v, replacements, compiled_regexp) for v in inputs_return
+            _multiple_replace(v, substitutions, compiled_regexp) for v in inputs_return
         ]
 
     return inputs_return
