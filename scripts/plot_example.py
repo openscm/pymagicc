71023ada37f8899d160165b602d9689d307acd24
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2018-10-21 23:08:00 +0200

    Add documentation

--- scripts/plot_example.py
+++ scripts/plot_example.py
@@ -1,8 +1,10 @@
 import os
 import matplotlib.pyplot as plt
-import pymagicc
 
+
+import pymagicc
 from pymagicc import scenarios
+
 
 plt.style.use("ggplot")
 plt.rcParams["figure.figsize"] = 10, 5
@@ -10,18 +12,30 @@ plt.rcParams["font.family"] = "serif"
 plt.rcParams["font.size"] = 12
 
 
-path = os.path.join(os.path.dirname(__file__),
-                    './example-plot.png')
+output_path = os.path.join(
+    os.path.dirname(__file__),
+    './example-plot.png'
+)
 
-for name, scen in sorted(scenarios.items(), reverse=True):
-    results, params = pymagicc.run(scen, return_config=True)
-    temp = (results["SURFACE_TEMP"].GLOBAL.loc[1850:] -
-            results["SURFACE_TEMP"].GLOBAL.loc[1850:1900].mean())
+for name, scen in scenarios.items():
+    results = pymagicc.run(scen)
+    results_df = results.df
+    results_df.set_index("time", inplace=True)
+
+    global_temp_time_rows = (
+        (results_df.variable == "Surface Temperature")
+        & (results_df.region == "World")
+    )
+
+    temp = (
+        results_df.value[global_temp_time_rows].loc[1850:]
+        - results_df.value[global_temp_time_rows].loc[1850:1900].mean()
+    )
     temp.plot(label=name)
 
 plt.legend()
 plt.title("Global Mean Temperature Projection")
-plt.ylabel(u"°C over pre-industrial (1850-1900 mean)")
+plt.ylabel("°C over pre-industrial (1850-1900 mean)");
 plt.legend(loc="best")
 
-plt.savefig(path, dpi=96)
+plt.savefig(output_path, dpi=96)
