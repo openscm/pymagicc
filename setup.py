2c127d848b30486ef3dfdbdc98213f801e16c005
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2018-10-24 23:41:02 +0200

    Add pyam as dependency

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ setup(
         ],
     },
     include_package_data=True,
-    install_requires=["pandas", "pandas-datapackage-reader", "f90nml"],
+    install_requires=["pandas", "pandas-datapackage-reader", "f90nml", "pyam-iamc"],
     tests_require=["pytest"],
     cmdclass=cmdclass,
 )
