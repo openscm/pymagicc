8b23264d611b9f26ce219734fb62481b717daf64
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2018-12-11 18:37:02 +1100

    Add test failures

--- tests/test_definitions.py
+++ tests/test_definitions.py
@@ -1,5 +1,9 @@
+import warnings
+
+
 import pytest
 import re
+
 
 from pymagicc.definitions import (
     convert_magicc6_to_magicc7_variables,
@@ -20,6 +24,8 @@ from pymagicc.definitions import (
 )
 def test_convert_magicc6_to_magicc7_variables(magicc6, magicc7):
     assert convert_magicc6_to_magicc7_variables(magicc6) == magicc7
+    assert convert_magicc6_to_magicc7_variables(magicc6.upper()) == magicc7
+    assert convert_magicc6_to_magicc7_variables(magicc6.lower()) == magicc7
 
 
 @pytest.mark.parametrize("magicc6", [("HFC-245ca"), ("HFC245ca")])
@@ -44,6 +50,8 @@ def test_convert_magicc6_to_magicc7_variables_hfc245ca_warning(magicc6):
 )
 def test_convert_magicc7_to_magicc6_variables(magicc7, magicc6):
     assert convert_magicc6_to_magicc7_variables(magicc7, inverse=True) == magicc6
+    assert convert_magicc6_to_magicc7_variables(magicc7.upper(), inverse=True) == magicc6
+    assert convert_magicc6_to_magicc7_variables(magicc7.lower(), inverse=True) == magicc6
 
 
 @pytest.mark.parametrize(
@@ -52,7 +60,33 @@ def test_convert_magicc7_to_magicc6_variables(magicc7, magicc6):
         ("HFC245FA_EMIS", "Emissions|HFC245fa"),
         ("CO2I_EMIS", "Emissions|CO2|MAGICC Fossil and Industrial"),
         ("CH4_EMIS", "Emissions|CH4"),
+        ("CH3CCl3_EMIS", "Emissions|CH3CCl3"),
+        ("CH3CCL3_EMIS", "Emissions|CH3CCl3"),
     ],
 )
 def test_convert_magicc7_to_openscm_variables(magicc7, openscm):
     assert convert_magicc7_to_openscm_variables(magicc7) == openscm
+    assert convert_magicc7_to_openscm_variables(magicc7.upper()) == openscm
+    assert convert_magicc7_to_openscm_variables(magicc7.lower()) == openscm
+
+
+@pytest.mark.parametrize(
+    "magicc7, openscm",
+    [
+        ("HFC245FA_EMIS", "Emissions|HFC245fa"),
+        ("CO2I_EMIS", "Emissions|CO2|MAGICC Fossil and Industrial"),
+        ("CH4_EMIS", "Emissions|CH4"),
+        ("CH3CCL3_EMIS", "Emissions|CH3CCl3"),
+    ],
+)
+def test_convert_openscm_to_magicc7_variables(magicc7, openscm):
+    assert convert_magicc7_to_openscm_variables(openscm, inverse=True) == magicc7
+    # OpenSCM variables are case sensitive hence this should not work
+    error_msg = re.escape(
+        "No conversion available for {'"
+        + '{}'.format(openscm.upper())
+        + "'}"
+    )
+    with pytest.raises(ValueError, match=error_msg):
+        convert_magicc7_to_openscm_variables(openscm.upper(), inverse=True)
+
