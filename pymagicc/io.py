7ccbaf12aa5a60ca53988be171c746fa741a3201
Jared Lewis <jared@jared.kiwi.nz>
2019-04-17 11:28:00 +1000

    Revert "Explicitly check for annualsteps == 1 now"
    This reverts commit 3aa21fd9427cffc2c1042ec755fb41c9db19280e.

--- pymagicc/io.py
+++ pymagicc/io.py
@@ -190,7 +190,8 @@ class _Reader(object):
             except TypeError:
                 metadata[metadata_key] = nml["THISFILE_SPECIFICATIONS"][k]
 
-        if "annualsteps" in metadata and metadata["annualsteps"] != 1:
+        # One input file has annualsteps == 0, but annual timesteps
+        if "annualsteps" in metadata and metadata["annualsteps"] > 1:
             raise InvalidTemporalResError(
                 "{}: Only annual files can currently be processed".format(self.filepath)
             )
