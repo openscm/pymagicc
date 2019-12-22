4e6c70d5f9a4226a992bed7c8edba6a69ac6d4b9
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2019-12-22 12:09:12 +1100

    Minor formatting fix

--- pymagicc/io.py
+++ pymagicc/io.py
@@ -82,16 +82,12 @@ def _unsupported_file(filepath):
 
 
 class NoReaderWriterError(ValueError):
-    """Exception raised when a valid Reader or Writer could not be found for the file
-    """
-
+    """Exception raised when a valid Reader or Writer could not be found for the file"""
     pass
 
 
 class InvalidTemporalResError(ValueError):
-    """Exception raised when a file has a temporal resolution which cannot be processed
-    """
-
+    """Exception raised when a file has a temporal resolution which cannot be processed"""
     pass
 
 
