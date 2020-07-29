f47a97b4f8a3c294c7f945a96f99f122e4148e54
Jared Lewis <jared@jared.kiwi.nz>
2020-07-29 12:09:00 +1000

    Add readers for v2 and legacy formats

--- pymagicc/io/binout.py
+++ pymagicc/io/binout.py
@@ -6,6 +6,7 @@ import pandas as pd
 from pymagicc.definitions import (
     convert_magicc6_to_magicc7_variables,
     convert_magicc7_to_openscm_variables,
+    convert_magicc_to_openscm_regions
 )
 from pymagicc.errors import InvalidTemporalResError
 from pymagicc.io.base import _Reader
@@ -48,41 +49,30 @@ class _BinData(object):
             return res[0]
         return res
 
+    def reset(self):
+        self.pos = 0
 
-class _BinaryOutReader(_Reader):
-    _regexp_capture_variable = re.compile(r"DAT\_(.*)\.BINOUT$")
-    _default_todo_fill_value = "N/A"
 
-    def read(self):
-        # Read the entire file into memory
-        data = _BinData(self.filepath)
+class _LegacyBinFormat:
+    version = None
 
-        metadata = self.process_header(data)
-        df, metadata, columns = self.process_data(data, metadata)
-
-        return metadata, df, columns
-
-    def process_header(self, data):
-        """
-        Reads the first part of the file to get some essential metadata
-
-        # Returns
-        return (dict): the metadata in the header
-        """
+    @staticmethod
+    def process_header(reader, stream):
         metadata = {
-            "datacolumns": data.read_chunk("I"),
-            "firstyear": data.read_chunk("I"),
-            "lastyear": data.read_chunk("I"),
-            "annualsteps": data.read_chunk("I"),
+            "datacolumns": stream.read_chunk("I"),
+            "firstyear": stream.read_chunk("I"),
+            "lastyear": stream.read_chunk("I"),
+            "annualsteps": stream.read_chunk("I"),
         }
         if metadata["annualsteps"] != 1:
             raise InvalidTemporalResError(
-                "{}: Only annual files can currently be processed".format(self.filepath)
+                "{}: Only annual files can currently be processed".format(reader.filepath)
             )
 
         return metadata
 
-    def process_data(self, stream, metadata):
+    @staticmethod
+    def process_data(reader, stream, metadata):
         """
         Extract the tabulated data from the input file
 
@@ -137,7 +127,7 @@ class _BinaryOutReader(_Reader):
         df.index.name = "time"
 
         variable = convert_magicc6_to_magicc7_variables(
-            self._get_variable_from_filepath()
+            reader._get_variable_from_filepath()
         )
         variable = convert_magicc7_to_openscm_variables(variable)
         column_headers = {
@@ -147,4 +137,120 @@ class _BinaryOutReader(_Reader):
             "todo": ["SET"] * len(regions),
         }
 
-        return df, metadata, self._set_column_defaults(column_headers)
+        return df, metadata, column_headers
+
+
+class _V2BinFormat(_LegacyBinFormat):
+    version = 2
+
+    @staticmethod
+    def process_variable(stream):
+        def _read_str(s):
+            return b"".join(s.read_chunk("c").tolist()).decode()
+
+        var_name = _read_str(stream)
+        region = _read_str(stream)
+        units = _read_str(stream)
+
+        data = stream.read_chunk("d")
+
+        return {"variable": var_name, "region": region, "unit": units}, data
+
+    @classmethod
+    def process_data(cls, reader, stream, metadata):
+        index = np.arange(metadata["firstyear"], metadata["lastyear"] + 1)
+
+        columns = {}
+        data = []
+        for i in range(metadata["datacolumns"]):
+            column_header, column_data = cls.process_variable(stream)
+
+            for k in column_header:
+                if k not in columns:
+                    columns[k] = []
+                columns[k].append(column_header[k])
+            data.append(column_data)
+
+        df = pd.DataFrame(np.asarray(data).T, index=index)
+
+        if isinstance(df.index, pd.core.indexes.numeric.Float64Index):
+            df.index = df.index.to_series().round(3)
+
+        df.index.name = "time"
+
+        # Convert the regions to openscm regions
+        columns["region"] = convert_magicc_to_openscm_regions(columns["region"])
+
+        # Convert the variable names to openscm variables
+        columns["variable"] = [
+            d[4:] if d.startswith("DAT_") else d for d in columns["variable"]
+        ]
+        columns["variable"] = convert_magicc6_to_magicc7_variables(columns["variable"])
+        columns["variable"] = convert_magicc7_to_openscm_variables(columns["variable"])
+
+        return df, metadata, columns
+
+
+def get_bin_format(version):
+    bin_formats = [
+        _LegacyBinFormat(),
+        _V2BinFormat()
+    ]
+
+    for f in bin_formats:
+        if f.version == version:
+            return f
+    raise ValueError("No formatter for binary version: {}".format(version))
+
+
+class _BinaryOutReader(_Reader):
+    _regexp_capture_variable = re.compile(r"DAT\_(.*)\.BINOUT$")
+    _default_todo_fill_value = "SET"
+
+    def _determine_bin_version(self, data):
+        try:
+            # Check the magicc magicc string
+            key = data.read_chunk("c")
+            if b"".join(key.tolist()) != b"magicc":
+                data.reset()  # Legacy files don't have a magic block - reset to start of file
+                return None
+            return data.read_chunk("h")
+        except TypeError:
+            # Wrong data type?
+            raise ValueError("{}: unexpected header format".format(self.filepath))
+
+    def read(self):
+        # Read the entire file into memory
+        data = _BinData(self.filepath)
+
+        file_version = self._determine_bin_version(data)
+        self.format = get_bin_format(file_version)
+
+        metadata = self.process_header(data)
+        df, metadata, columns = self.process_data(data, metadata)
+
+        return metadata, df, columns
+
+    def process_header(self, data):
+        return self.format.process_header(self, data)
+
+    def process_data(self, stream, metadata):
+        """
+        Extract the tabulated data from the input file
+
+        # Arguments
+        stream (Streamlike object): A Streamlike object (nominally StringIO)
+            containing the table to be extracted
+        metadata (dict): metadata read in from the header and the namelist
+
+        # Returns
+        df (pandas.DataFrame): contains the data, processed to the standard
+            MAGICCData format
+        metadata (dict): updated metadata based on the processing performed
+        """
+        df, metadata, columns = self.format.process_data(self, stream, metadata)
+
+        if "todo" not in columns:
+            columns["todo"] = self._default_todo_fill_value
+
+        return df, metadata, self._set_column_defaults(columns)
