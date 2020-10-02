eb536caa2f4466fa62b3090788e02a02cc9ab84c
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2020-10-02 12:30:03 +1000

    Pin black

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ REQUIREMENTS_DEPLOY = ["setuptools>=38.6.0", "twine>=1.11.0", "wheel>=0.31.0"]
 REQUIREMENTS_DEV = (
     [
         "bandit",
-        "black",
+        "black==19.10b0",
         "black-nb",
         "flake8",
         "isort>=5",
