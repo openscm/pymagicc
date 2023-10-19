a7811a58490f450a7278210ad7d15030885d274e
Philip Hackstock <20710924+phackstock@users.noreply.github.com>
2023-10-19 14:08:00 +0200

    Update python version to 3.9>=py<=3.11

--- setup.py
+++ setup.py
@@ -41,9 +41,9 @@ CLASSIFIERS = [
     "Intended Audience :: Developers",
     "License :: OSI Approved :: BSD License",
     "Operating System :: OS Independent",
-    "Programming Language :: Python :: 3.6",
-    "Programming Language :: Python :: 3.7",
-    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11",
 ]
 REQUIREMENTS_INSTALL = [
     "pandas-datapackage-reader",
